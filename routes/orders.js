8555d545f0d95cc30a9016be82a6e8b7e5f87792
Digi18 <me.digvijay18@gmail.com>
2019-12-25 20:42:49 +0530

    orders route added

---
+++ routes/orders.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const dotEnv = require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+
+const dburl = process.env.URL;
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:true}));
+
+router.post('/order',(req,res) => {
+
+   MongoClient.connect(dburl,{useNewUrlParser:true,useUnifiedTopology:true},(err,client) => {
+
+            let data = {
+
+                	id:req.body.id,
+                	market:req.body.market,
+                	product_no:req.body.product,
+                	cost:req.body.cost
+                }
+
+                if(err){
+                	console.log("Error",err);
+                }
+                else{
+
+                	let coll = client.db('Aamku').collection('Orders');
+
+                	coll.insertOne(data,function(err,resp) => {
+
+                          if(err){
+                          	consloe.log("Error",err);
+                          }
+                          else{
+
+                          	res.send("Order generated");
+                          	client.close();
+                          }
+                	});
+                }
+
+   });
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(require('./routes/saveSalesperson.js'));
 app.use(require('./routes/generateClient.js'));
 app.use(require('./routes/checkRole.js'));
 app.use(require('./routes/getProducts.js'));
+app.use(require('./routes/orders.js'));
 
 const port = process.env.PORT || 3000;
 
